dfb118bfb2e38ba4d763ef30e6c11e0685663ab2
sheilaghv <132933201+sheilaghv@users.noreply.github.com>
2023-05-15 16:54:08 -0300

    a

--- js/15_5.js
+++ js/15_5.js
@@ -2,7 +2,10 @@ function ejemplo(params){
  let inp = document.querySelector("#entrada");
  let p = document.querySelector("#salida");
  let valor = inp.value;
+ let divImg = document.querySelector("#img");
  p.innerText=valor;
+ divImg.innerHTML= '<img src="../img/michilogo.jpg" alt= />';
+
  console.log(valor);
  console.dir(inp);
  console.log(p.value);
